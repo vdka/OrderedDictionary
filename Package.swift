0419c4344265a02ca542f6ecb2ca570d2917257e
vdka <ethanjackwitz@gmail.com>
2017-10-11 11:45:34 +1000

    Added Package.swift for Swift 4

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "OrderedDictionary",
+    dependencies: [],
+    targets: [
+        .target(name: "OrderedDictionary", dependencies: []),
+    ],
+    swiftLanguageVersions: [4]
+)
